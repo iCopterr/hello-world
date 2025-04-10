7bfd2e0b6ea0b3ba6683f96c853697b8dec76e3b
iCopterr <zxcopterzx1@gmail.com>
2025-04-10 21:42:54 +0700

    Create main.java

---
+++ main.java
@@ -0,0 +1,20 @@
+public class zXyZ_314159{
+
+public static void main(String[] args){
+int [] lolz = {5,3,8,1,2};
+sortmeh(lolz);
+
+for(int p = 0;p < lolz.length; p++)
+System.out.print(lolz[p] + " ");
+}
+
+public static void sortmeh(int[] whaaat){
+for(int i=0;i<whaaat.length-1;i++)
+{
+for(int j=0;j<whaaat.length;j++){
+if(whaaat[j]<whaaat[i]){
+int temp = whaaat[i];
+whaaat[i] = whaaat[j];
+whaaat[i] = temp;  // BUG: should be whaaat[j] = temp
+}
+} } }
